94b00a9ff80ad3331fda2aab23d9c2747f241bb9
Alexey <root@anakros.me>
2019-02-03 00:45:17 +0700

    total upload field

--- init.js
+++ init.js
@@ -1,5 +1,14 @@
 plugin.loadLang();
 
+function formatBytes(bytes,decimals) {
+   if(bytes == 0) return '0.00 KiB';
+   var k = 1024,
+       dm = decimals <= 0 ? 0 : decimals || 2,
+       sizes = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PB', 'EB', 'ZB', 'YB'],
+       i = Math.floor(Math.log(bytes) / Math.log(k));
+   return parseFloat((bytes / Math.pow(k, i)).toLocaleString(undefined, {minimumFractionDigits: dm, maximumFractionDigits: dm})) + ' ' + sizes[i];
+}
+
 if(plugin.canChangeColumns())
 {
 	plugin.config = theWebUI.config;
@@ -7,6 +16,7 @@ if(plugin.canChangeColumns())
 	{
 		this.tables.trt.columns.push({text: 'DlgRatio', width: '100px', id: 'dratio', type: TYPE_NUMBER});
 		this.tables.trt.columns.push({text: 'TotalRatio', width: '100px', id: 'tratio', type: TYPE_NUMBER});
+		this.tables.trt.columns.push({text: 'TotalUpload', width: '100px', id: 'tupload', type: TYPE_NUMBER});
 		plugin.trtFormat = this.tables.trt.format;
 		plugin.config.call(this,data);
 		plugin.reqId1 = theRequestManager.addRequest("trt", theRequestManager.map("d.get_custom=")+"deluge_ratio",function(hash,torrent,value)
@@ -18,6 +28,11 @@ if(plugin.canChangeColumns())
 			total = Number(value)/1000 + Number(torrent.dratio);
 			torrent.tratio = total.toLocaleString(undefined, {minimumFractionDigits: 3});
 		});
+		plugin.reqId3 = theRequestManager.addRequest("trt", theRequestManager.map("d.get_size_bytes="),function(hash,torrent,value)
+		{
+			total = Number(torrent.tratio) * value;
+			torrent.tupload = formatBytes(total, 2);
+		});
 		plugin.trtRenameColumn();
 	}
 
@@ -27,6 +42,7 @@ if(plugin.canChangeColumns())
 		{
 			theWebUI.getTable("trt").renameColumnById("dratio",theUILang.dratio);
 			theWebUI.getTable("trt").renameColumnById("tratio",theUILang.tratio);
+			theWebUI.getTable("trt").renameColumnById("tupload",theUILang.tupload);
 			if(thePlugins.isInstalled("rss"))
 				plugin.rssRenameColumn();
 			if(thePlugins.isInstalled("extsearch"))
@@ -42,6 +58,7 @@ if(plugin.canChangeColumns())
 		{
 			theWebUI.getTable("rss").renameColumnById("dratio",theUILang.dratio);
 			theWebUI.getTable("rss").renameColumnById("tratio",theUILang.tratio);
+			theWebUI.getTable("rss").renameColumnById("tupload",theUILang.tupload);
 		}
 		else
 			setTimeout(arguments.callee,1000);
@@ -53,6 +70,7 @@ if(plugin.canChangeColumns())
 		{
 			theWebUI.getTable("teg").renameColumnById("dratio",theUILang.dratio);
 			theWebUI.getTable("teg").renameColumnById("tratio",theUILang.tratio);
+			theWebUI.getTable("teg").renameColumnById("tupload",theUILang.tupload);
 		}
 		else
 			setTimeout(arguments.callee,1000);
@@ -63,11 +81,14 @@ plugin.onRemove = function()
 {
 	theWebUI.getTable("trt").removeColumnById("dratio");
 	theWebUI.getTable("trt").removeColumnById("tratio");
+	theWebUI.getTable("trt").removeColumnById("tupload");
 	if(thePlugins.isInstalled("rss"))
 	{
 		theWebUI.getTable("rss").removeColumnById("dratio");
 		theWebUI.getTable("rss").removeColumnById("tratio");
+		theWebUI.getTable("rss").removeColumnById("tupload");
 	}
 	theRequestManager.removeRequest( "trt", plugin.reqId1 );
 	theRequestManager.removeRequest( "trt", plugin.reqId2 );
+	theRequestManager.removeRequest( "trt", plugin.reqId3 );
 }
